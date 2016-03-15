60a84c403100841417a92892d6361e2539c9d724
Hagane <deemson@gmail.com>
2016-03-15 06:34:39 +0600

    abstracted build task

---
+++ Drek/BuildTasks.fs
@@ -0,0 +1,34 @@
+﻿namespace Drek
+
+open System;
+open System.Diagnostics;
+open System.IO;
+
+type FSharpBuild(target: Target, sources: List<string>, references: List<string>, output: string) =
+    inherit BuildTask(target, sources, references, output)
+
+    override this.Construct(target) (sources) (references) (output) =
+        FSharpBuild(target, sources, references, output) :> BuildTask
+
+    override this.Execute(context) =
+        let targetStr = 
+            match target with
+            | ClassLibrary -> "library"
+            | Executable -> "exe"
+        let extension =
+            match target with
+            | ClassLibrary -> "dll"
+            | Executable -> "exe"
+        let outFile = output + "/" + context + "." + extension
+
+        let fsharpHome = Environment.GetEnvironmentVariable("FSHARP_HOME")
+        let command = sprintf "\"%s/fsc.exe\"" fsharpHome
+        let args = (sprintf " --target:%s" targetStr) + (sprintf " -o:%s" outFile)
+        let sourceFiles = String.concat " " sources
+        ignore (Directory.CreateDirectory output)
+        let proc = Process.Start(command, sprintf "%s %s" args sourceFiles)
+        proc.WaitForExit();
+        match proc.ExitCode with
+        | 0 -> "OK"
+        | _ -> "ERROR"
+
--- Drek/Task.fs
+++ Drek/Task.fs
@@ -5,24 +5,26 @@ open System.Diagnostics
 open System.IO
 
 type ExecutionContext = string
+type Target = | Executable | ClassLibrary
 
 [<AbstractClassAttribute>]
 type Task(name: string) =
     member this.name = name
     abstract member Execute: ExecutionContext -> string
 
-type Target = | Executable | ClassLibrary
+[<AbstractClassAttribute>]
 type BuildTask(target: Target, sources: List<string>, references: List<string>, output: string) = 
     inherit Task("build")
     new (target: Target) = BuildTask(target, [], [], "./target/")
+    abstract member Construct: Target -> List<string> -> List<string> -> string -> BuildTask
 
     member this.AddFile(file: string) = this.AddFiles [file]
-    member this.AddFiles(files: List<string>) = BuildTask(target, sources @ files, references, output)
+    member this.AddFiles(files: List<string>) = this.Construct target (sources @ files) references output
 
     member this.AddReference(ref: string) = this.AddReferences [ref]
-    member this.AddReferences(refs: List<string>) = BuildTask(target, sources, references @ refs, output)
+    member this.AddReferences(refs: List<string>) = this.Construct target sources (references @ refs) output
 
-    member this.WithOutput(output: string) = BuildTask(target, sources, references, output)
+    member this.WithOutput(output: string) = this.Construct target sources references output
 
     static member (<+) (task: BuildTask, file: string) = task.AddFile file 
     static member (<++) (task: BuildTask, files: List<string>) = task.AddFiles files
@@ -30,26 +32,6 @@ type BuildTask(target: Target, sources: List<string>, references: List<string>, 
     static member (<?) (task: BuildTask, ref: string) = task.AddReference ref
 
     static member (=>) (task: BuildTask, output: string) = task.WithOutput output
-
-    override this.Execute(context) =
-        let targetStr = 
-            match target with
-            | ClassLibrary -> "library"
-            | Executable -> "exe"
-        let extension =
-            match target with
-            | ClassLibrary -> "dll"
-            | Executable -> "exe"
-        let outFile = output + "/" + context + "." + extension
-
-        let fsharpHome = Environment.GetEnvironmentVariable("FSHARP_HOME")
-        let command = sprintf "\"%s/fsc.exe\"" fsharpHome
-        let args = (sprintf " --target:%s" targetStr) + (sprintf " -o:%s" outFile)
-        let sourceFiles = String.concat " " sources
-        ignore (Directory.CreateDirectory output)
-        let proc = Process.Start(command, sprintf "%s %s" args sourceFiles)
-        proc.WaitForExit();
-        "OK"
 
 type CleanTask(cleanDirs: List<string>) =
     inherit Task("clean")
